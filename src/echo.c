0014465377751accc9f5b8739de0b5adca48dd6c
cospplredman <cospplredman@gmail.com>
2023-06-25 13:20:19 -0500

    added more error checking

--- src/echo.c
+++ src/echo.c
@@ -1,1 +1,1 @@
-main(c,v)char**v;{for(c>1&&printf("%s",v++[1]);*++v;)if(printf(" %s",*v)<0)exit(1);exit(puts("")<0);}
+main(c,v)char**v;{for(c>1&&printf("%s",v++[1])<0?exit(1):0;*++v;)if(printf(" %s",*v)<0)exit(1);exit(puts("")<0);}
