eb6d7afe1f390bfe0a618f11ec3c4a4f547213b1
cospplredman <cospplredman@gmail.com>
2023-06-25 20:08:41 -0500

    added -- support

--- src/basename.c
+++ src/basename.c
@@ -1,1 +1,9 @@
-main(c,v)char**v;{if(c>3|c<2)exit(1);char*p=v[1],*e=p+strlen(p),*q=p;for(;--e>q&*e==47;*e=0);for(;q<e;p=*q++-47?p:q);c>2&&!strcmp(v[2],e-=strlen(v[2])-1)&e>p?*e=0:0;exit(puts(p)<0);}
+main(c,v)char**v;{
+	if(c>1&!strcmp(v[1],"--"))c--,v++;
+	if(c>3|c<2)exit(1);
+	char*p=v[1],*e=p+strlen(p),*q=p;
+	for(;--e>q&*e==47;*e=0);
+	for(;q<e;p=*q++-47?p:q);
+	c>2&&!strcmp(v[2],e-=strlen(v[2])-1)&e>p?*e=0:0;
+	exit(puts(p)<0);
+}
--- src/dirname.c
+++ src/dirname.c
@@ -1,2 +1,9 @@
 #define X(a)for(;--e>p&*e a'/';*e=0);
-main(c,v)char**v;{if(c-2)exit(1);char*p=v[1],*e=p+strlen(p);X(==)X(!=)if(e==p)exit(puts(".")<0);X(==)e[1]=0;exit(puts(*p?p:"/"));}
+main(c,v)char**v;{
+	if(c>1&!strcmp(v[1],"--"))c--,v++;
+	if(c-2)exit(1);
+	char*p=v[1],*e=p+strlen(p);
+	X(==)X(!=)if(e==p)exit(puts(".")<0);
+	X(==)e[1]=0;
+	exit(puts(*p?p:"/"));
+}
