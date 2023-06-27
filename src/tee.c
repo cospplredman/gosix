70e28c8efaf75a5e6992ca9a6198f24793c13da2
cospplredman <cospplredman@gmail.com>
2023-06-26 19:57:46 -0500

    smallification (golfed it down a bit)

--- src/tee.c
+++ src/tee.c
@@ -1,28 +1,13 @@
 #include<stdio.h>
 #include<signal.h>
-//TODO arguments
-char*s="w";
-i=0,j;d;main(c,v)char**v;{
-	FILE*f[c];
-
-	for(;++i<c&&*v[i]==45;){
-		j++;
-		if(v[i][j] == 'i'){
-			i--;
-			signal(SIGINT,SIG_IGN);
-		}else if(v[i][j] == 'a'){
-			i--;
+char*s="w";i=1,j;d;e;main(c,v)char**v;{
+	for(;i<c&&*v[i]==45;)switch(e=v[i][++j]){
+		case'i':
+			signal(SIGINT,SIG_IGN);break;
+		case'a':
 			s="a";
-		}else{
-			if(j==1||v[i][j])
-				exit(1);
-			j=0;
-		}
-	}
-
-	if(setvbuf(stdin,NULL,_IONBF,1))exit(1);
+	default:if(j==1||e)exit(1);j=0;i++;}
+	FILE*f[c];if(setvbuf(stdin,NULL,_IONBF,1))exit(1);
 	for(j=--i,f[j]=stdout;++i<c;)f[i]=fopen(v[i],s);
-	for(;(d=getc(stdin))>=0;)
-		for(i=j;i<c;i++)
-			if(f[i])putc(d,f[i]),fflush(f[i]);
+	for(;(d=getc(stdin))>=0;)for(i=j;i<c;i++)if(f[i])putc(d,f[i]),fflush(f[i]);
 }
