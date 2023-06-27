6c33fa5e8e2f995e4c5b8902ab19f59893cca762
cospplredman <cospplredman@gmail.com>
2023-06-26 19:16:10 -0500

    made tee more posix compliant (added some flags)

--- src/tee.c
+++ src/tee.c
@@ -1,12 +1,28 @@
 #include<stdio.h>
+#include<signal.h>
 //TODO arguments
-i;d;main(c,v)char**v;{
-	FILE*f[c];*f=stdout;
-	if(setvbuf(stdin,NULL,_IONBF,1))exit(1);
-	for(;++i<c;)f[i]=fopen(v[i],"w");
-	for(;(d=getc(stdin))>=0;)
-		for(i=0;i<c;i++){
-			if(setvbuf(f[i],NULL,_IONBF,1))exit(1);
-			putc(d,f[i]);
+char*s="w";
+i=0,j;d;main(c,v)char**v;{
+	FILE*f[c];
+
+	for(;++i<c&&*v[i]==45;){
+		j++;
+		if(v[i][j] == 'i'){
+			i--;
+			signal(SIGINT,SIG_IGN);
+		}else if(v[i][j] == 'a'){
+			i--;
+			s="a";
+		}else{
+			if(j==1||v[i][j])
+				exit(1);
+			j=0;
 		}
+	}
+
+	if(setvbuf(stdin,NULL,_IONBF,1))exit(1);
+	for(j=--i,f[j]=stdout;++i<c;)f[i]=fopen(v[i],s);
+	for(;(d=getc(stdin))>=0;)
+		for(i=j;i<c;i++)
+			if(f[i])putc(d,f[i]),fflush(f[i]);
 }
