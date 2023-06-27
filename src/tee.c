f6da4e87b9b1a94dc6a4b935f5b6c1c9ec7d04ee
cospplredman <cospplredman@gmail.com>
2023-06-26 20:05:11 -0500

    smallification

--- src/tee.c
+++ src/tee.c
@@ -1,12 +1,7 @@
 #include<stdio.h>
 #include<signal.h>
 char*s="w";i=1,j;d;e;main(c,v)char**v;{
-	for(;i<c&&*v[i]==45;)switch(e=v[i][++j]){
-		case'i':
-			signal(SIGINT,SIG_IGN);break;
-		case'a':
-			s="a";
-	default:if(j==1||e)exit(1);j=0;i++;}
+	for(;i<c&&*v[i]==45;)e=v[i][++j],e=='i'?(signal(SIGINT,SIG_IGN),0):e=='a'?s="a":j==1||e?(exit(1),0):(j=0,i++);
 	FILE*f[c];if(setvbuf(stdin,NULL,_IONBF,1))exit(1);
 	for(j=--i,f[j]=stdout;++i<c;)f[i]=fopen(v[i],s);
 	for(;(d=getc(stdin))>=0;)for(i=j;i<c;i++)if(f[i])putc(d,f[i]),fflush(f[i]);
