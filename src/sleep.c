1b34181286f8b81b1b5faab64762fe5b1fe4b65a
cospplredman <cospplredman@gmail.com>
2023-06-29 07:02:20 -0500

    fixed scanf validation

--- src/sleep.c
+++ src/sleep.c
@@ -1,6 +1,6 @@
 #include<time.h>
 j;main(c,v)char**v;{
 	if(c>1&!strcmp(v[1],"--"))c--,v++;
-	if(c-2|sscanf(v[1],"%d",&j)==strlen(v[1]))exit(1);
+	if(c-2&&sscanf(v[1],"%d",&j)-strlen(v[1]))exit(1);
 	for(time_t s=time(NULL);time(NULL)-s<j+1;);exit(0);
 }
