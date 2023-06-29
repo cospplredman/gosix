9e1eaa3456e94dc6005bfaeb930c779c320ae11a
cospplredman <cospplredman@gmail.com>
2023-06-29 07:01:57 -0500

    fixed a bug in dirname involving single character file paths

--- src/dirname.c
+++ src/dirname.c
@@ -1,9 +1,10 @@
-#define X(a)for(;--e>p&'/'a*e;*e=0);
+#define X(a)for(;--e>p&'/'a*e;*e=0);e++;
 main(c,v)char**v;{
 	if(c>1&&!strcmp(v[1],"--"))c--,v++;
 	if(c-2)exit(1);
 	char*p=v[1],*e=p+strlen(p);
-	X(==)X(!=)if(e==p)exit(puts(".")<0);
+	X(==)X(!=)
+	if(e-1==p&&e[-1]-47)exit(puts(".")<0);
 	X(==)e[1]=0;
 	exit(puts(*p?p:"/"));
 }
