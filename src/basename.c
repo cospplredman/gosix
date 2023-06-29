87cb26108aa8f0ddec502e8073a68523d49171fa
cospplredman <cospplredman@gmail.com>
2023-06-29 07:00:37 -0500

    replaced & with && to prevent SEGFAULT

--- src/basename.c
+++ src/basename.c
@@ -1,5 +1,5 @@
 main(c,v)char**v;{
-	if(c>1&!strcmp(v[1],"--"))c--,v++;
+	if(c>1&&!strcmp(v[1],"--"))c--,v++;
 	if(c>3|c<2)exit(1);
 	char*p=v[1],*e=p+strlen(p),*q=p;
 	for(;--e>q&*e==47;*e=0);
--- src/dirname.c
+++ src/dirname.c
@@ -1,6 +1,6 @@
 #define X(a)for(;--e>p&'/'a*e;*e=0);
 main(c,v)char**v;{
-	if(c>1&!strcmp(v[1],"--"))c--,v++;
+	if(c>1&&!strcmp(v[1],"--"))c--,v++;
 	if(c-2)exit(1);
 	char*p=v[1],*e=p+strlen(p);
 	X(==)X(!=)if(e==p)exit(puts(".")<0);
